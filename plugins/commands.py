1bfc740db92689216730424a057ccbb45c791380
Personal Robot <86073822+Agoriadmin@users.noreply.github.com>
2022-05-08 20:53:19 +0530

    Update and rename commands.py to commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,7 +17,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+                InlineKeyboardButton('🤖 Updates', url='https://t.me/personal_privetbot1')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
